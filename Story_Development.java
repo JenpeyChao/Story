16bfeecc3e943b6685ac6a4c53295ef39905a9f4
aaron <redskin.rox@gmail.com>
2024-05-23 14:54:13 -0400

    creating the rooms

--- Story_Development.java
+++ Story_Development.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 
 public class Story_Development {
-    public class Items{
+
+    public static class Items{
         private String name;
         private String description;
         private boolean take = false;
@@ -10,33 +11,48 @@ public class Story_Development {
         public Items(String name,String description,boolean take){
             this.name = name;
             this.description = description;
-            this.take = take;
             this.used = false;
+            
         }
         public void setUsed(boolean used){
             this.used = true;
         }
     }
 
-    public class Rooms{
-        Rooms north;
-        Rooms east;
-        Rooms south;
-        Rooms west;
-        String description = "";
-        Items[] items;
+    public static class Rooms{
+        private Rooms north;
+        private Rooms east;
+        private Rooms south;
+        private Rooms west;
+        private String description = "";
+        private Items[] items;
+        private String examine;
+
+        public Rooms(String description, String examine, Items[] items){
+            this.north = null;
+            this.east = null;
+            this.south = null;
+            this.west = null;
+            this.description = description;
+            this.items = items;
+            this.examine = examine;
+        }
     }
     
     static Rooms createStory(){
-        Rooms story = new Rooms();
-        story.items = new Items[2];
-        story.description = "In front of you is a bookshelf filled with books and random potions. \nYou see a locked door to the East. \nYou see a alter with a sock engraving on it.";
+        String description = "In front of you is a bookshelf filled with books and random potions. \nYou see a locked door to the East. \nYou see an alter with a sock engraving on it.";
+        String examine = "You look closely at the bookshelf. You see a lot of different books and potions. \n You avoid the potions since they seem harmful.\nHowever you see a key poking out of a book";
+        Items[] items = new Items[1];
+        items[0] = new Items("key","A shiny key to an unknown door or chest",true);
+        Rooms story = new Rooms(description,examine, items);
+
+        
         return story;
     }
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         Rooms story = createStory();
-        Items backpack = new Items();
+        // Items backpack = new Items();
 
         System.out.println("Type something to start");
         String start = myObj.nextLine();
@@ -44,22 +60,14 @@ public class Story_Development {
         do{
             System.out.println(story.description);
             System.out.println("Here are your options:");
-            if(story.north!= null){
-                System.out.println("1) Go North");
-            }
-            if(story.east!= null){
-                System.out.println("2) Go East");
-            }
-            if(story.south!= null){
-                System.out.println("3) Go South");
-            }
-            if(story.west!= null){
-                System.out.println("4) Go West");
-            }
-
-            
+            System.out.println("1) Go North");
+            System.out.println("2) Go East");
+            System.out.println("3) Go South");
+            System.out.println("4) Go West");
+            System.out.println("5) Examine the current room");
+            System.out.println("6) Take an item");
 
 
-        }while(start);
+        }while(!start.equals("") || );
     }
 }
