d00d055572f796ab480d8839b2b0c5e6d9bc60e2
aaron <redskin.rox@gmail.com>
2024-05-24 13:25:58 -0400

    finished makign the case for taking an item and checking inventory

--- Story_Development.java
+++ Story_Development.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Story_Development {
@@ -5,20 +7,25 @@ public class Story_Development {
     public static class Items{
         private String name;
         private String description;
-        private boolean take = false;
-        private boolean used = false;
+
         
-        public Items(String name,String description,boolean take){
+        
+        public Items(String name,String description){
             this.name = name;
             this.description = description;
-            this.used = false;
+            
             
         }
-        public void setUsed(boolean used){
-            this.used = true;
+        public Items(){
+            this.name = "";
+            this.description = "";
+            
         }
+        // public void setUsed(boolean used){
+        //     this.used = true;
+        // }
         public String getName(){
-            return name;
+            return this.name;
         }
     }
 
@@ -43,6 +50,7 @@ public class Story_Development {
             this.items = items;
             this.examine = examine;
             this.key = key;
+            this.locked = locked;
         }
         public void printItems(){
             if (this.items.length == 0){
@@ -53,6 +61,27 @@ public class Story_Development {
                 }
             }
             
+        }
+        public Items getItem(String name){
+            for (int i =0;i < this.items.length;i++){
+                if (this.items[i].getName().equals(name)){
+                    return items[i];
+                }
+            }
+            return new Items();
+        }
+        public String[] getItemName(){
+            String[] itemsName = new String[this.items.length];
+            for(int i = 0; i < this.items.length; i++){
+                itemsName[i] = this.items[i].getName();
+            }
+            return itemsName;
+        }
+        public boolean isLocked(){
+            return this.locked;
+        }
+        public void getKey(){
+            System.out.println(this.key);
         }
         public void getDescription(){
             System.out.println(this.description);
@@ -90,19 +119,19 @@ public class Story_Development {
         String description = "In front of you is a bookshelf filled with books and random potions. \nYou see a locked door to the East. \nYou see an alter with a sock engraving on it.";
         String examine = "You look closely at the bookshelf. You see a lot of different books and potions. \n You avoid the potions since they seem harmful.\nHowever you see a key poking out of a book";
         Items[] items = new Items[1];
-        items[0] = new Items("Bloody Key","A bloody key to an unknown door or chest",true);
+        items[0] = new Items("bloody key","A bloody key to an unknown door or chest");
         Rooms main = new Rooms(description, examine, items, false, "");
 
         description = "Youre in a dark long hall way. With the exit to the west";
         examine = "As you look down the hallway, you can see a small light flickering";
         items = new Items[0];
-        Rooms locked_door = new Rooms(description, examine, items, true, "Bloody Key");
+        Rooms locked_door = new Rooms(description, examine, items, true, "bloody key");
         main.setEast(locked_door);
         locked_door.setWest(main);
 
         items = new Items[0];
         description = "Congratulations youve found the exit to the mad lair";
-        Rooms exit = new Rooms(description, "", items, true, "Sock","");
+        Rooms exit = new Rooms(description, "", items, true, "sock");
         main.setSouth(exit);
         exit.setNorth(main);
 
@@ -116,7 +145,7 @@ public class Story_Development {
         description = "Youre in a dim lit room. \nYou see a hole in the wall thats too small to fit your hand in.";
         examine = "As you look closely at the sign, you can see something dripping all over the sign.";
         items = new Items[1];
-        items[0] = new Items("Sock","A random sock that, you dont want to wear",true);
+        items[0] = new Items("sock","A random sock that, you dont want to wear");
         Rooms board = new Rooms(description, examine, items, false, "");
         hall.setEast(board);
         board.setWest(hall);
@@ -124,15 +153,58 @@ public class Story_Development {
         
         return main;
     }
+    
     public static void main(String[] args) {
         Scanner myObj = new Scanner(System.in);
         Rooms story = createStory();
-        // Items backpack = new Items();
-
+        ArrayList<String> backpack = new ArrayList<String>();
         System.out.println("Type something to start");
         String start = myObj.nextLine();
         System.out.println("You've woken up in cave like lair. As you look around, you see a board with a name 'Harry Potter'. It seems youre trapped in a mad wizards lair");
         do{//swtich case with a 2 word case; ex: examine room
+            start = myObj.nextLine();
+            String[] choice = start.split(" ");
+            
+            switch (choice[0]) {
+                case "take":
+                    String item;
+                    if (choice.length > 2){
+                        item = choice[1]+" "+choice[2];
+                    }else{
+                        item = choice[1];
+                    }
+                    
+                    if(Arrays.stream(story.getItemName()).anyMatch(item::equals)){
+                        backpack.add(item);
+
+                    }else{
+                        System.out.println("You rip a brain cell out of your head");
+                    }
+                    break;
+                case "move":
+                    break;
+                case "inventory":
+                case "backpack":
+                    if (backpack.size() <=0){
+                        System.out.println("You have nothing in your bag");
+                    }
+                    for(int i = 0; i < backpack.size();i++){
+                        System.out.println(backpack.get(i));
+                    }
+                    break;
+                case "go":
+                    
+                    break;
+                default:
+                    // System.out.println("Here are your options:");
+                    // System.out.println("1) Go North");
+                    // System.out.println("2) Go East");
+                    // System.out.println("3) Go South");
+                    // System.out.println("4) Go West");
+                    // System.out.println("5) Examine the current room");
+                    // System.out.println("6) Take (item)");
+                    break;
+            }
             // story.getDescription();
             // story.printItems();
             // story = story.getEast();
@@ -141,14 +213,8 @@ public class Story_Development {
             // story = story.getWest();
             // story.getDescription();
             // story.printItems();
-            // System.out.println("Here are your options:");
-            // System.out.println("1) Go North");
-            // System.out.println("2) Go East");
-            // System.out.println("3) Go South");
-            // System.out.println("4) Go West");
-            // System.out.println("5) Examine the current room");
-            // System.out.println("6) Take an item");
-            break;
+            
+            
 
         }while(!start.equals("") );
 
